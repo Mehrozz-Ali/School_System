f1e860fba05c13f9052954e53b25e67c9423de13
Mehrozz-Ali <mehrozali912@gmail.com>
2026-08-05 09:54:50 +0500

    Attendance and percentage of student charts are complete

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,47 @@
-const Admin = () => {
+import UserCard from "@/components/UserCard";
+import CountChart from "@/components/CountChart";
+import AttendenceChart from "@/components/AttendenceChart";
+import FinanceChart from "@/components/FinanceChart";
+
+const AdminPage = () => {
   return (
-    <div className=''>Admin</div>
+    <div className='p-4 flex gap-4 flex-col md:flex-row'>
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* user Card */}
+        <div className="flex gap-4 justify-between flex-wrap ">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+
+        {/* Middle Charts */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendenceChart />
+          </div>
+
+        </div>
+
+        {/* BOTTOM CHART */}
+        <div className="w-full h-[500px]">
+          <FinanceChart />
+        </div>
+
+
+      </div>
+
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3">r</div>
+
+
+    </div>
   )
 }
 
-export default Admin
+export default AdminPage
---
+++ src/components/FinanceChart.tsx
@@ -0,0 +1,8 @@
+"use client"
+const FinanceChart = () => {
+  return (
+    <div>FinanceChart</div>
+  )
+}
+
+export default FinanceChart
