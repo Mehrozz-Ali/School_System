ebccf93cc87cdbf48933bdcfdcd10eaa2d244781
Mehrozz-Ali <mehrozali912@gmail.com>
2026-08-14 10:19:18 +0500

    countChart get data dynamically

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,9 +1,9 @@
 import UserCard from "@/components/UserCard";
-import CountChart from "@/components/CountChart";
 import AttendenceChart from "@/components/AttendenceChart";
 import FinanceChart from "@/components/FinanceChart";
 import EventCalendar from "@/components/EventCalendar";
 import Announcements from "@/components/Announcements";
+import CountChartContainer from "@/components/CountChartContainer";
 
 const AdminPage = () => {
   return (
@@ -12,16 +12,16 @@ const AdminPage = () => {
       <div className="w-full lg:w-2/3 flex flex-col gap-8">
         {/* user Card */}
         <div className="flex gap-4 justify-between flex-wrap ">
-          <UserCard type="student" />
+          <UserCard type="admin" />
           <UserCard type="teacher" />
+          <UserCard type="student" />
           <UserCard type="parent" />
-          <UserCard type="staff" />
         </div>
 
         {/* Middle Charts */}
         <div className="flex gap-4 flex-col lg:flex-row">
           <div className="w-full lg:w-1/3 h-[450px]">
-            <CountChart />
+            <CountChartContainer />
           </div>
 
           <div className="w-full lg:w-2/3 h-[450px]">
--- src/components/CountChart.tsx
+++ src/components/CountChart.tsx
@@ -3,71 +3,48 @@ import Image from 'next/image';
 import { RadialBarChart, RadialBar, Legend, Tooltip } from 'recharts';
 
 
-// #region Sample data
-const data = [
-    {
-        name: 'Total ',
-        count: 106,
-        fill: 'white',
-    },
-    {
-        name: 'Girls',
-        count: 53,
-        fill: '#FAF27C',
-    },
-    {
-        name: 'Boys',
-        count: 53,
-        fill: '#C3EBFA',
-    },
-];
 
 
 
 
 
-const CountChart = () => {
+const CountChart = ({ boys, girls }: { boys: number, girls: number }) => {
+
+    const data = [
+        {
+            name: 'Total ',
+            count: boys + girls,
+            fill: 'white',
+        },
+        {
+            name: 'Girls',
+            count: girls,
+            fill: '#FAF27C',
+        },
+        {
+            name: 'Boys',
+            count: boys,
+            fill: '#C3EBFA',
+        },
+    ];
+
     return (
-        <div className='bg-white rounded-xl w-full h-full p-4'>
-            {/* Title */}
-            <div className="flex justify-between items-center">
-                <h1 className="text-lg font-semibold">Students</h1>
-                <Image src="/moreDark.png" alt="" width={20} height={20} />
-            </div>
+        <div className="relative w-full h-[70%] flex justify-center items-center">
 
-            {/* CHART */}
-            <div className="relative w-full h-[70%] flex justify-center items-center">
-                <RadialBarChart
-                    responsive
-                    style={{ width: "100%", height: "100%" }}
-                    data={data}
-                    cx="50%"
-                    cy="50%"
-                    innerRadius="40%"
-                    outerRadius="90%"
-                    barSize={30}
-                >
-                    <RadialBar background dataKey="count" />
-                </RadialBarChart>
+            <RadialBarChart
+                responsive
+                style={{ width: "100%", height: "100%" }}
+                data={data}
+                cx="50%"
+                cy="50%"
+                innerRadius="40%"
+                outerRadius="90%"
+                barSize={30}
+            >
+                <RadialBar background dataKey="count" />
+            </RadialBarChart>
 
-                <Image src="/maleFemale.png" alt=""width={50} height={50} className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
-            </div>
-
-            {/* BOTTOM */}
-            <div className="flex justify-center gap-16">
-                <div className="flex flex-col gap-1">
-                    <div className="w-5 h-5 bg-lamaSky rounded-full " />
-                    <h1 className="font-bold ">1,234</h1>
-                    <h1 className="text-sm text-gray-300">Boys [55%]</h1>
-                </div>
-
-                <div className="flex flex-col gap-1">
-                    <div className="w-5 h-5 bg-lamaYellow rounded-full " />
-                    <h1 className="font-bold ">1,234</h1>
-                    <h1 className="text-sm text-gray-300">Girls [45%]</h1>
-                </div>
-            </div>
-
+            <Image src="/maleFemale.png" alt="" width={50} height={50} className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
         </div>
     )
 }
